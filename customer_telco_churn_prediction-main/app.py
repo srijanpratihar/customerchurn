44afe160f1f455e37926f7552fd99531000d5d10
srijanpratihar <100072338+srijanpratihar@users.noreply.github.com>
2022-06-29 12:26:47 +0530

    Update app.py

--- customer_telco_churn_prediction-main/app.py
+++ customer_telco_churn_prediction-main/app.py
@@ -8,23 +8,76 @@ import numpy as np
 st.title("Welcome to flower prediction app")
 model=load_model('churn.h5')   
 
-gender =(int)(st.number_input("enter gender"))
-seniorcitizen=(int)(st.number_input("senior citizen"))
-partner=(int)(st.number_input("partner"))
-dependents=(int)(st.number_input("dependents"))
-tenure=(float)(st.number_input("tenure"))
-phoneservice=(int)(st.number_input("phoneservice"))
-MultipleLines=(int)(st.number_input("MultipleLines"))
-OnlineSecurity=(int)(st.number_input("OnlineSecurity"))
-OnlineBackup=(int)(st.number_input("OnlineBackup"))
-DeviceProtection=(int)(st.number_input("DeviceProtection"))
-TechSupport=(int)(st.number_input("TechSupport"))
-StreamingTV=(int)(st.number_input("StreamingTV"))
-StreamingMovies=(int)(st.number_input("StreamingMovies")) 
+gender=st.selectbox('select gender',('Male','Female'))
+if(gender=='Male'):
+        gender=0
+else:
+        gender=1
+seniorcitizen=st.selectbox('select senior citizen or not',('Yes','No'))
+if(seniorcitizen=='Yes'):
+        seniorcitizen=1
+else:
+        seniorcitizen=0
+partner=st.selectbox('do you have a partner',('Yes','No'))
+if(partner=='Yes'):
+        partner=1
+else:
+        partner=0
+dependents=st.selectbox('do you have any dependents',('Yes','No'))
+if(dependents=='Yes'):
+        dependents=1
+else:
+        dependents=0
+tenure=(float)(st.number_input("enter your tenure"))
+
+phoneservice=st.selectbox('do you have phone service',('Yes','No'))
+if(phoneservice=='Yes'):
+        phoneservice=1
+else:
+        phoneservice=0
+MultipleLines=st.selectbox('do you have multiple lines',('Yes','No'))
+if(MultipleLines=='Yes'):
+        MultipleLines=1
+else:
+        MultipleLines=0
+OnlineSecurity=st.selectbox('do you have online security',('Yes','No'))
+if(OnlineSecurity=='Yes'):
+        OnlineSecurity=1
+else:
+        OnlineSecurity=0
+OnlineBackup=st.selectbox('do you have online backup',('Yes','No'))
+if(OnlineBackup=='Yes'):
+        OnlineBackup=1
+else:
+        OnlineBackup=0
+DeviceProtection=st.selectbox('do you have Device protection',('Yes','No'))
+if(DeviceProtection=='Yes'):
+        DeviceProtection=1
+else:
+        DeviceProtection=0
+TechSupport=st.selectbox('do you have tech support',('Yes','No'))
+if(TechSupport=='Yes'):
+        TechSupport=1
+else:
+        TechSupport=0
+StreamingTV=st.selectbox('do you stream TV',('Yes','No'))
+if(StreamingTV=='Yes'):
+        StreamingTV=1
+else:
+        StreamingTV=0
+StreamingMovies=st.selectbox('do you stream Movies',('Yes','No'))
+if(StreamingMovies=='Yes'):
+        StreamingMovies=1
+else:
+        StreamingMovies=0
 InternetService=st.text_input("InternetService")#1
 Contract=st.text_input("Contract") #2
 PaymentMethod=st.text_input("PaymentMethod")#3
-PaperlessBilling=(int)(st.number_input("PaperlessBilling"))
+PaperlessBilling=st.selectbox('do you have paperless Billing',('Yes','No'))
+if(PaperlessBilling=='Yes'):
+        PaperlessBilling=1
+else:
+        PaperlessBilling=0
 MonthlyCharges=(float)(st.number_input("MonthlyCharges"))
 TotalCharges=(float)(st.number_input("TotalCharges"))
 
